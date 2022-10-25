15f61899334509e7bbec7be91fcc6bbe215a97b6
Mikhail Zaitsau <mikhail_zaitsau@outlook.com>
2022-10-25 18:21:57 +0300

    Add edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+       @article = Article.find(params[:id]) 
+    end
+
     def create
         @article = Article.new(article_params)
 
@@ -17,10 +21,20 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+                redirect_to @article
+        else
+            render "edit"
+        end
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
-
+    
     private
         def article_params
             params.require(:article).permit(:title, :text)
