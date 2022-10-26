1c7f257806bf63cc2af6dd36d11ac5555f5447a5
root <root@DWCH.localdomain>
2022-10-27 02:29:04 +0300

    Adding user to articles complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_26_192910) do
+ActiveRecord::Schema.define(version: 2022_10_26_232151) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
