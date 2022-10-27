f4c224d4321b0b9adfb88bb999378a8b76870f5c
root <root@DWCH.localdomain>
2022-10-27 22:40:48 +0300

    Add user to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
     private
         def comment_params
             params.require(:comment).permit(:commenter,
-            :body)
+            :body, :user_id)
         end
 
 end
